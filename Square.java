45f43cbaa4b526cce6a252ab114c8f4217bccdf8
ShivanagowdaR <rshivanagowda22@gmail.com>
2026-01-30 07:20:43 +0530

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,8 @@
+public class Square {
+    public static void main(String[] args) {
+        double side = 5.0;
+        double area = side * side;
+        System.out.println("The side length is: " + side);
+        System.out.println("The area of the square is: " + area);
+    }
+}
